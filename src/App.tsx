97f6f2f1d5783b7ecdd7aa8cecea809e0634c0f3
徐阳杰 <xuyangjie@xuyangjiedeMacBook-Pro.local>
2019-08-21 09:27:22 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Button ,message} from 'antd';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  onSubmit(){
+    message.success('hahahah')
+  }
+  render(){
+    return (
+      <div className="App">
+      <Button type="primary" onClick={this.onSubmit}>Button</Button>
+     </div>
+    )
+  }
 }
+
+
 
 export default App;
